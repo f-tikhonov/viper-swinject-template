0bf8b35c8825db233ff92fa59992147e755d04df
Fedor Tikhonov <qperegrine@gmail.com>
2018-05-22 01:04:12 +0300

    ___Module___Repository files

---
+++ VIPER.xctemplate/Default/___FILEBASENAME___Repository.swift
@@ -0,0 +1,29 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created ___FULLUSERNAME___ on ___DATE___.
+//  Copyright © ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+import Foundation
+
+protocol ___VARIABLE_productName:identifier___RepositoryInput: class {
+
+
+}
+
+final class ___VARIABLE_productName:identifier___Repository: ___VARIABLE_productName:identifier___RepositoryInput {
+
+    init() {
+        #if DEBUG
+            print("___VARIABLE_productName:identifier___Repository init()")
+        #endif
+    }
+
+    deinit {
+        #if DEBUG
+        print("___VARIABLE_productName:identifier___Repository deinit()")
+        #endif
+    }
+}
