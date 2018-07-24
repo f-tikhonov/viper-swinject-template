a42fd703a233a881dd7b4c03c3b681f19c9f9913
Fedor Tikhonov <qperegrine@gmail.com>
2018-07-24 19:45:17 +0300

    [Classic VIPER] file <Module>Protocols added

---
+++ VIPER.xctemplate/Classic VIPER/___FILEBASENAME___Protocols.swift
@@ -0,0 +1,32 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created ___FULLUSERNAME___ on ___DATE___.
+//  Copyright © ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+import Foundation
+
+// MARK: Interactor
+protocol ___VARIABLE_productName:identifier___InteractorInput: class {
+    
+}
+
+protocol ___VARIABLE_productName:identifier___InteractorOutput: class {
+   
+}
+
+// MARK: - Router
+protocol ___VARIABLE_productName:identifier___RouterInput: class {
+    
+}
+
+// MARK: View Controller
+protocol ___VARIABLE_productName:identifier___ViewInput: class {
+    
+}
+
+protocol ___VARIABLE_productName:identifier___ViewOutput: class {
+    func viewIsReady()
+}
